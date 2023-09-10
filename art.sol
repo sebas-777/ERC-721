64d569990da54d35a26d34d8dc86eef3068710c7
sebas-777 <sebastiancast06@hotmail.com>
2023-09-10 12:33:37 -0500

    initial declaration

---
+++ art.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0; 
+
+import"@openzeppelin/contracts@4.4.2/token/ERC721/ERC721.sol";
+import"@openzeppelin/contracts@4.4.2/access/Ownable.sol"; 
+
+
+contract ArtToken is ERC721,Ownable { 
+
+    //============================================================
+    // Initial Statements
+    //============================================================
+
+    //Smart Contract Constructor 
+    constructor(string memory _name, string memory _symbol) 
+        ERC721(_name, _symbol){} 
+
+    // NFT token counter
+    uint256 COUNTER;
+
+    //Pricing of NFT Tokens ( price of the artWork)
+    uint256 fee = 5 ether;
+
+    // Data structure with the properties of the artwork 
+    struct Art{
+        string name;
+        uint256 id;
+        uint256 dna;
+        uint8 level;
+        uint8 rarity;
+    } 
+
+    //Storage structure for keeping artiworks
+    Art[] public art_works;
+
+    //Declaration of an event 
+    event NewArtWork(address indexed owner,uint256 id,uint256 dna); 
+
+    //============================================================
+    // Help functions
+    //============================================================ 
+
+    
+
+    
+}
